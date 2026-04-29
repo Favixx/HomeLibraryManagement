baf71d358bdb7d246e089f84e6dc7d39a5add423
Favixx <bogdandom52@gmail.com>
2026-04-29 15:19:09 +0200

    29.04.2026 BD

--- manage_books/models.py
+++ manage_books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import pytz
 
 # Create your models here.
 class Book(models.Model):
@@ -27,19 +28,46 @@ class Book(models.Model):
     language = models.CharField(max_length=20, choices=LANGUAGES)
     is_read = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
-
+    authors = models.ManyToManyField('Author', related_name='books', blank=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.RESTRICT)
+    series = models.ForeignKey('Series', on_delete=models.RESTRICT, blank=True, null=True)
+    genres = models.ManyToManyField('Genre', related_name='books', blank=True)
+    topics = models.ManyToManyField('Topic', related_name='books', blank=True)
 
 class Author(models.Model):
+    TITLES = [
+            ('ks', "Ks."),
+            ('dr', "Dr."),
+            ('prof', "Prof."),
+            ('mr', "Mr."),
+            ('ms', "Ms."),
+        ]
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    title = models.CharField(max_length=20, choices=TITLES, blank=True, null=True)
+    alias = models.CharField(max_length=100, blank=True, null=True)
 
 class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=2, choices=pytz.country_names.items())
+    founded_year = models.IntegerField()
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    # books = models.ManyToOneField(Book, related_name='publisher')
 
 class Genre(models.Model):
+    name = models.CharField(max_length=100)
 
 class Series(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
 
 class Topic(models.Model):
-
-class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
 class Note(models.Model):
-
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,)
